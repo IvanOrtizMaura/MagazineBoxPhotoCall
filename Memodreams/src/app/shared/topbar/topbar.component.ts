6957dfc366c884da8dbd34094f29b4b2f63cd186
ivan.ortiz <ivan.ortiz@pmcs.de>
2025-07-19 02:41:14 +0200

    feat(Topbar): :sparkles: Implement topbar

--- Memodreams/src/app/shared/topbar/topbar.component.ts
+++ Memodreams/src/app/shared/topbar/topbar.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
+import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-import { NgIf } from '@angular/common';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -11,22 +11,38 @@ import { MenuItem } from 'primeng/api';
   imports: [
     RouterModule,
     MenubarModule,
+    SidebarModule,
     ButtonModule,
-    NgIf
+    
   ],
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent {
-  menuItems: MenuItem[] = [];
+  sidebarVisible = false;
 
-  ngOnInit() {
-    this.menuItems = [
-      { label: 'INICIO', routerLink: '/' },
-      { label: 'SERVICIOS', routerLink: '/servicios' },
-      { label: 'EVENTOS', routerLink: '/eventos' },
-      { label: 'GALERÍA', routerLink: '/galeria' },
-      { label: 'CONTACTO', routerLink: '/contacto' }
-    ];
+  menuItems: MenuItem[] = [
+    { label: 'INICIO', routerLink: '/' },
+    { label: 'SERVICIOS', routerLink: '/servicios' },
+    { label: 'EVENTOS', routerLink: '/eventos' },
+    { label: 'GALERÍA', routerLink: '/galeria' },
+    { label: 'CONTACTO', routerLink: '/contacto' }
+  ];
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
   }
-}
+
+  openSocialMedia(socialMedia: string) {
+    switch (socialMedia) {
+      case 'facebook':
+        window.open('https://www.facebook.com/memodreamsevents', '_blank');
+        break;
+      case 'instagram':
+        window.open('https://www.instagram.com/memodreamsevents', '_blank');
+        break;
+      case 'tiktok':
+        window.open('https://www.tiktok.com/@memodreamsevents', '_blank');
+        break;
+    }
+  }}
